a7c2229964877e371101450c38257f7963dff848
Official-BigT <triumphanyanga@gmail.com>
2025-09-22 16:58:02 +0100

    Update API base URL to point to the production server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5680/api/v1", //backend
+  baseURL: "https://sage-sync-backend.onrender.com/api/v1", //backend
   withCredentials: true, // ensures cookies are sent/received
   headers: {
     "Content-Type": "application/json",
