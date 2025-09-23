458e318313dce8bd46f3c0adfb02f0863a5e9249
Official-BigT <triumphanyanga@gmail.com>
2025-09-23 14:13:14 +0100

    Add .env file with API base URL configuration

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://sage-sync-backend.onrender.com/api/v1", //backend
+  baseURL: import.meta.env.VITE_API_URL, //USE ENV VARIABLES
+  //baseURL: "https://sage-sync-backend.onrender.com/api/v1", //backend
   withCredentials: true, // ensures cookies are sent/received
   headers: {
     "Content-Type": "application/json",
